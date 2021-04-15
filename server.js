34b2514025fef44c236ecac70fa2284dbb34bbcf
Dipesh Maywade <dipeshmaywade@gmail.com>
2021-04-15 17:12:43 +0530

    Added[feat]: ability to GET data from mySQL DB

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const mysql = require('mysql');
+const express = require('express');
+var app = express();
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json());
 
 var mysqlConnection = mysql.createConnection({
     host: 'localhost',
@@ -13,4 +18,19 @@ mysqlConnection.connect((error)=> {
     }else{
         console.log("DB Connection Failed: "+JSON.stringify(error, undefined,2));
     }
+});
+
+app.listen(3000, () => {
+    console.log("Express server is Runing at Port: 3000");
+});
+
+//Get all 
+app.get('/employees', (req, res)=>{
+    mysqlConnection.query('select * from employee',(err, rows, filed)=>{
+        if (!err) {
+            res.send(rows)
+        }else{
+            res.send(err);
+        }
+    })
 });
